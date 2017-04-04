7f96f647dac333746ec30e289245cb1f874cf9f4
aaron huynh <huynh.aaron@gmail.com>
2017-04-03 23:27:51 -0700

    Add djangoRESTframework

--- carsforme/settings.py
+++ carsforme/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'webpack_loader',
+    'rest_framework',
     'car'
 ]
 
@@ -51,6 +52,13 @@ WEBPACK_LOADER = {
         'BUNDLE_DIR_NAME': 'bundles/',
         'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
     }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAdminUser',
+    ],
+    'PAGE_SIZE': 10
 }
 
 MIDDLEWARE = [
