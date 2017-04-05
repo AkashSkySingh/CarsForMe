2471db616794ad4521683e378e865e86c8defd5f
aaron huynh <huynh.aaron@gmail.com>
2017-04-05 15:14:19 -0700

    Add react files

--- assets/js/Components/navbar/navbar.jsx
+++ assets/js/Components/navbar/navbar.jsx
@@ -4,11 +4,10 @@ import React from 'react';
 
 class Navbar extends React.Component {
 
-
-
   render() {
     return (
       <div>
+        <h2> Cars For Me</h2>
         <img src="https://www.carthrottle.com/wp-content/uploads/2013/11/Picture-106.png" alt="P1"/>
       </div>
     )
