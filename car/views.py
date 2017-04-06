f7b0437931a6ad380cb270ac31e748b870b6ae74
App Academy Student <akashpreetsingh91@gmail.com>
2017-04-06 12:47:00 -0700

    removal of functionality as it does not work on python3

--- car/views.py
+++ car/views.py
@@ -28,59 +28,47 @@ class TrimViewSet(viewsets.ModelViewSet):
     serializer_class = CarModelSerializer
     filter_backends = (filters.DjangoFilterBackend,)
 
-QUERY="query"
 
-MODEL_MAP = {
-    Trim: [
-    "model_make_id",
-    "model_make_id",
-    "model_name",
-    "model_trim",
-    "model_year",
-    "model_body",
-    "model_engine_position",
-    "model_engine_cc",
-    "model_engine_cyl",
-    "model_engine_type",
-    "model_engine_valves_per_cyl",
-    "model_engine_power_ps",
-    "model_engine_power_rpm",
-    "model_engine_torque_nm",
-    "model_engine_torque_rpm",
-    "model_engine_bore_mm",
-    "model_engine_stroke_mm",
-    "model_engine_compression",
-    "model_engine_fuel",
-    "model_top_speed_kph",
-    "model_0_to_100_kph",
-    "model_drive",
-    "model_transmission_type",
-    "model_seats",
-    "model_doors",
-    "model_weight_kg",
-    "model_length_mm",
-    "model_width_mm",
-    "model_height_mm",
-    "model_wheelbase_mm",
-    "model_lkm_hwy",
-    "model_lkm_mixed",
-    "model_lkm_city",
-    "model_fuel_cap_l",
-    "model_sold_in_us",
-    "model_co2",
-    "model_make_display",
-    "make_display",
-    "make_country"
-    ]
-}
-
-def search(request):
-    object = []
-    for model,fields in MODEL_MAP.iteritems():
-        objects+=generic_search(request,model,fields,QUERY)
-        return render_to_response("search_results.html",
-                                {
-                                "objects":objects,
-                                "search_string": request.GET.get(QUERY, "")
-                                }
-        )
+# MODEL_MAP = {
+#     Trim: [
+#     "model_make_id",
+#     "model_make_id",
+#     "model_name",
+#     "model_trim",
+#     "model_year",
+#     "model_body",
+#     "model_engine_position",
+#     "model_engine_cc",
+#     "model_engine_cyl",
+#     "model_engine_type",
+#     "model_engine_valves_per_cyl",
+#     "model_engine_power_ps",
+#     "model_engine_power_rpm",
+#     "model_engine_torque_nm",
+#     "model_engine_torque_rpm",
+#     "model_engine_bore_mm",
+#     "model_engine_stroke_mm",
+#     "model_engine_compression",
+#     "model_engine_fuel",
+#     "model_top_speed_kph",
+#     "model_0_to_100_kph",
+#     "model_drive",
+#     "model_transmission_type",
+#     "model_seats",
+#     "model_doors",
+#     "model_weight_kg",
+#     "model_length_mm",
+#     "model_width_mm",
+#     "model_height_mm",
+#     "model_wheelbase_mm",
+#     "model_lkm_hwy",
+#     "model_lkm_mixed",
+#     "model_lkm_city",
+#     "model_fuel_cap_l",
+#     "model_sold_in_us",
+#     "model_co2",
+#     "model_make_display",
+#     "make_display",
+#     "make_country"
+#     ]
+# }
