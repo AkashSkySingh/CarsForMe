47a689325c2dadccec5ba3e23111624c1c8b4f37
App Academy Student <akashpreetsingh91@gmail.com>
2017-04-06 11:15:23 -0700

    Revisions with addition to search functionality for Trim

--- assets/js/components/app.js
+++ assets/js/components/app.js
@@ -1,8 +1,10 @@
 import React from 'react';
-
+import NavBar from './navbar/navbar';
 
 const App = ({ children }) => (
   <div >
+    <h1>Hi from APP</h1>
+    <NavBar />
     { children }
   </div>
 );
