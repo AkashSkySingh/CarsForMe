b6fa34aea891146cfcd5ccc3e01ba9d30b0fd81f
Khalil Nasirov <khalil.nasirov@gmail.com>
2017-04-06 11:06:23 -0700

    basic react setup

---
+++ assets/js/reducers/root_reducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+// import carDetailReducer from './car_detail_reducer';
+
+const rootReducer = combineReducers({
+  // carDetail: carDetailReducer,
+});
+
+export default rootReducer;
