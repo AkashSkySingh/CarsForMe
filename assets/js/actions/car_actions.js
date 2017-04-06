6910bf0fcd2d23780925fc2639d204ced6aabc4e
Khalil Nasirov <khalil.nasirov@gmail.com>
2017-04-06 12:03:09 -0700

    fix minor problems

---
+++ assets/js/actions/car_actions.js
@@ -0,0 +1,12 @@
+import * as CarAPIUtil from '../util/car_api_util';
+
+export const RECEIVE_CAR = 'RECEIVE_CAR';
+
+export const receiveCar = car => ({
+  type: RECEIVE_CAR,
+  car
+});
+
+export const fetchCar = car => dispatch => (
+  CarAPIUtil.fetchCar(car).then(res => dispatch(receiveCar(res)))
+);
