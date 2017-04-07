918938fc1fbc905aaf0cc0ff33f8f15f4d9187f6
aaron huynh <huynh.aaron@gmail.com>
2017-04-07 12:41:37 -0700

    google image

--- assets/js/components/navbar/navbar.jsx
+++ assets/js/components/navbar/navbar.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+class Navbar extends Component {
 
+  constructor(){
+    super();
+    this.state = { link: null };
+  }
 
-class Navbar extends React.Component {
+  componentDidMount(){
+    let that = this;
+    $.ajax({
+      type: 'GET',
+      url: 'https://www.googleapis.com/customsearch/v1?key=AIzaSyCzWOe2mBvhD-gSITuOQc_oMKY1bSx91IY&cx=003264831380035041777:hi4v6rphnr8&q=2017%20Ford%20Focus%20RS&searchType=image&imgSize=large&alt=json',
+      success: function(data){
+        that.setState({link: data.items[0].link});
+      }
+    })
+
+  }
+
   render() {
     return (
       <div>
         <h2> Cars For Me</h2>
         <img src="https://www.carthrottle.com/wp-content/uploads/2013/11/Picture-106.png" alt="P1"/>
+        {this.state.link ? <img width="600" src={`${this.state.link}`} /> : "Error: No Car Selected" }
       </div>
-    );
+    )
   }
 }
 
