8f008cc842379834ea4b07463921084b7169874b
App Academy Student <akashpreetsingh91@gmail.com>
2017-04-07 10:57:49 -0700

    Revisions to SplashContainer

--- assets/js/components/navbar/navbar.jsx
+++ assets/js/components/navbar/navbar.jsx
@@ -9,7 +9,7 @@ class Navbar extends React.Component {
         <h2> Cars For Me</h2>
         <img src="https://www.carthrottle.com/wp-content/uploads/2013/11/Picture-106.png" alt="P1"/>
       </div>
-    )
+    );
   }
 }
 
--- assets/js/index.jsx
+++ assets/js/index.jsx
@@ -4,12 +4,14 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import Root from './components/root';
 import configureStore from './store/store';
 
-import { fetchCar } from './util/car_api_util';
+import { fetchCars } from './util/car_api_util';
 
-window.fetchCar = fetchCar;
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
+
+  window.store = store;
+  window.fetchCars = fetchCars;
 });
