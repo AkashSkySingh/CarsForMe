c06305ddb4f8920b9a8766936e295d85a8440fc9
aaron huynh <huynh.aaron@gmail.com>
2017-04-09 18:26:09 -0700

    Fix React 15.5.2 and maps and pictures

--- car/static/assets/js/components/car_detail/car_detail.jsx
+++ car/static/assets/js/components/car_detail/car_detail.jsx
@@ -9,22 +9,21 @@ class CarDetail extends React.Component {
   constructor(props) {
     super(props);
     this.state = { picture: null, position: "unknown", lng: null, lat: null, locations: {} };
+    this.getInfo = this.getInfo.bind(this);
   }
-
-
   componentWillMount() {
-    this.props.fetchCar({id: this.props.id});
+    this.props.fetchCar(this.props.id).then(this.getInfo);
   }
-
   componentWillReceiveProps(nextProps) {
     if (this.props.id !== nextProps.id) {
-      this.props.fetchCar(nextProps.id);
+      this.props.fetchCar(nextProps.id).then(this.getInfo);
     }
   }
-
-  componentDidMount() {
+  getInfo() {
+    this.getPosition();
     this.getCarPicture();
-    // this.getDealerships();
+    this.getDealerships();
+    console.log(this.props);
   }
 
   getPosition() {
@@ -34,7 +33,7 @@ class CarDetail extends React.Component {
                      JSON.stringify(position.coords.longitude);
         this.setState({position: latlng, lat: position.coords.latitude, lng: position.coords.longitude });
       },
-      (error) => alert(JSON.stringify(error)),
+      (error) => {},
       {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
     );
   };
@@ -71,15 +70,15 @@ class CarDetail extends React.Component {
       },
       url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json`,
       success: function(data){
-        debugger;
         that.setState({locations: data.results});
       }
     });
   }
 
   render() {
+    this.getPosition();
     const { details } = this.props;
-    console.log(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.773972,-122.431297&radius=300000&keyword=${this.props.details.model_make_id}%20Dealership&type=car_deal&key=AIzaSyC2ysLHnXB5uOYcbrMyrAbwNqxziomWUIs`);
+    // console.log(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.773972,-122.431297&radius=300000&keyword=${this.props.details.model_make_id}%20Dealership&type=car_deal&key=AIzaSyC2ysLHnXB5uOYcbrMyrAbwNqxziomWUIs`);
     let markers;
     // let text;
     if (this.state.locations.length !== undefined) {
@@ -143,8 +142,10 @@ class CarDetail extends React.Component {
             height={'500px'}
             lat={this.state.lat}
             lng={this.state.lng}
-            zoom={12}
+            zoom={10}
             loadingMessage={`Loading Map Nearest Dealerships`}
+            params={params}
+            onMapCreated={this.onMapCreated}
             >
 
               {markers}
